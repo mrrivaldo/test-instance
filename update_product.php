13f784fd9642485a969877ff675edc32343fa231
Rivaldo <rivaldorizky77@gmail.com>
2023-12-20 15:49:15 +0800

    update

--- update_product.php
+++ update_product.php
@@ -19,13 +19,16 @@ $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 // Check if a new image is uploaded
 if (!empty($image['name'])) {
     // Process image upload logic (You can include your S3 upload code here)
-    // ...
+    $targetDir = "uploads/";
+    $targetFile = $targetDir . basename($image['name']);
+    move_uploaded_file($image['tmp_name'], $targetFile);
 
     // Update product data in RDS with the new image URL
+    $newImageUrl = $targetFile;
     $sql = "UPDATE products SET name='$name', description='$description', price=$price, image_url='$newImageUrl' WHERE product_id=$id";
 } else {
     // Update product data in RDS without updating the image
-    $sql = "UPDATE products SET name='$name', description='$description', price=$price, image_url='$existingImage' WHERE product_id=$id";
+    $sql = "UPDATE products SET name='$name', description='$description', price=$price WHERE product_id=$id";
 }
 
 $result = mysqli_query($connection, $sql);
