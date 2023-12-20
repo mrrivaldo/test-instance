11d6c6c20c51de89f8f3280bc6a3839c66b8e5c6
Rivaldo <rivaldorizky77@gmail.com>
2023-12-20 15:19:27 +0800

    update db

--- db.inc.php
+++ db.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'db-web.cmjrai3ugqxl.us-east-1.rds.amazonaws.com');
+define('DB_SERVER', 'db-webv2.cmjrai3ugqxl.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'admin101');
 define('DB_DATABASE', 'db_wipe');
