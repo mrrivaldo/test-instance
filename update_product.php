855c4e761b80ff361563ff5df2ef9534496d9e45
Rivaldo <rivaldorizky77@gmail.com>
2023-12-23 18:55:05 +0800

    s3

--- update_product.php
+++ update_product.php
@@ -5,6 +5,8 @@ ini_set('display_errors', 1);
 require './vendor/autoload.php';
 include "db.inc.php";
 
+use Aws\S3\S3Client;
+
 // Retrieve form data
 $id = $_POST['id'];
 $name = $_POST['name'];
@@ -18,7 +20,7 @@ $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 // Check if a new image is uploaded
 if (!empty($image['name'])) {
-    // Process image upload logic (You can include your S3 upload code here)
+    // Process image upload logic for RDS
     $targetDir = "./images/";
     $targetFile = $targetDir . basename($image['name']);
     move_uploaded_file($image['tmp_name'], $targetFile);
@@ -26,6 +28,33 @@ if (!empty($image['name'])) {
     // Update product data in RDS with the new image URL
     $newImageUrl = $targetFile;
     $sql = "UPDATE products SET name='$name', description='$description', price=$price, image_url='$newImageUrl' WHERE product_id=$id";
+
+    // Upload the image to S3
+    $s3 = new S3Client([
+        'version' => 'latest',
+        'region' => 'us-east-1', // Replace with your AWS region
+        'credentials' => [
+            'key' => getenv('AWS_ACCESS_KEY_ID'),
+            'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+        ],
+    ]);
+
+    $bucket = 'wipe-web-s3'; // Replace with your S3 bucket name
+    $s3Key = basename($newImageUrl);
+
+    try {
+        $result = $s3->putObject([
+            'Bucket' => $bucket,
+            'Key' => $s3Key,
+            'SourceFile' => $newImageUrl,
+        ]);
+
+        // Optionally, you can delete the local image file after uploading to S3
+        unlink($newImageUrl);
+    } catch (Exception $e) {
+        // Handle S3 upload error
+        echo "Error uploading to S3: " . $e->getMessage();
+    }
 } else {
     // Update product data in RDS without updating the image
     $sql = "UPDATE products SET name='$name', description='$description', price=$price WHERE product_id=$id";
